e9458616837dbd96e4318f9ace37d8a827740335
little-silk <gabbyalvarez@comcast.net>
2022-05-25 12:14:43 -0700

    Updated design

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // My Scripts
 
-console.log("test");
+const circle = new CircleType(document.getElementById('circle'))
+  .radius(180);
+document.getElementById('destroyButton')
+  .addEventListener('click', circle.destroy.bind(circle));
+
+const btn = document.getElementById('destroyButton');
+
+btn.addEventListener('click', () => {
+  btn.style.display = 'none';
+});
